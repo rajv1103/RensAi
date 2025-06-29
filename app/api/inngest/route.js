2e875098af8c3698da2152b70f5b8f2b912816e3
Raaj Verma <rajverma49999@gmail.com>
2025-06-29 15:12:56 +0530

    fixm

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,8 @@
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights} from "@/lib/inngest/function";
+import { generateIndustryInsights } from "@/lib/inngest/function";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-     generateIndustryInsights,
-  ],
+  functions: [generateIndustryInsights],
 });
